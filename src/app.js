d3dea50f13525a48eaeabdba2313b8d533241c49
Dhayana N. Silva <dhayanadesenvolvimento@gmail.com>
2025-04-05 23:42:58 -0300

    refatora codigo

--- src/app.js
+++ src/app.js
@@ -7,46 +7,52 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const EMAIL_USUARIO = process.env.EMAIL_USUARIO;
-const EMAIL_SENHA = process.env.EMAIL_SENHA;
+const EMAIL_USER = process.env.EMAIL_USER;
+const EMAIL_PASSWORD = process.env.EMAIL_PASSWORD;
 
 app.use(cors());
 app.use(express.json());
 
 app.post("/contato", async (req, res) => {
-  const { nome, email, mensagem } = req.body;
+  const { name, email, message } = req.body;
 
-  if (!nome || !email || !mensagem) {
+  if (!name || !email || !message) {
     return res.status(400).json({ error: "Todos os campos são obrigatórios." });
   }
 
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587, 
+      port: 587,
       secure: false,
       auth: {
-        user: EMAIL_USUARIO,
-        pass: EMAIL_SENHA,
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD,
       },
     });
 
     const mailOptions = {
       from: email,
-      to: EMAIL_USUARIO,
-      subject: `Mensagem de ${nome} - ${email}`,
-      text: mensagem,
+      to: EMAIL_USER,
+      subject: `Mensagem de ${name}, através do portifólio pessoal`,
+      text: message,
     };
 
     await transporter.sendMail(mailOptions);
     transporter.close();
 
-    res.status(200).json({ message: "Email enviado com sucesso!" });
-     
+    res
+      .status(200)
+      .json({ message: "Mensagem enviada com sucesso, retornarei em breve." });
   } catch (error) {
     console.error("Erro ao enviar email:", error);
     transporter.close();
-    res.status(500).json({ error: "Erro ao enviar email." });
+    res
+      .status(500)
+      .json({
+        error:
+          "Erro ao enviar email! Verifique as informações fornecidas ou tente novamente mais tarde.",
+      });
   }
 });
 
